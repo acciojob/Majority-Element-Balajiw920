df3cbcdc680b365621ef3535d9acb31684bfb521
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 04:57:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
 //your code here
-function majorityElement(arr) {
-  let count = 1;
-  let majority = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] === majority) {
-      count++;
-    } else {
-      count--;
-      if (count === 0) {
-        majority = arr[i];
-        count = 1;
-      }
+function majorityElement(nums) {
+    const n = nums.length;
+    const freqMap = new Map();
+    
+    for(let i=0; i<n; i++) {
+        freqMap.set(nums[i], freqMap.get(nums[i]) + 1 || 1);
+        if(freqMap.get(nums[i]) > Math.floor(n/2)) {
+            return nums[i];
+        }
     }
-  }
-
-  // Count the occurrences of majority element
-  count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === majority) {
-      count++;
-    }
-  }
-
-  // Check if majority element appears more than floor(n/2) times
-  if (count > Math.floor(arr.length / 2)) {
-    return majority;
-  } else {
-    return -1;
-  }
 }
