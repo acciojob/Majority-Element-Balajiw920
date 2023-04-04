01eb956ec90fc933394ae30ba8b727a8eba56a6f
Balajiw920 <wbalaji2021@gmail.com>
2023-04-04 23:47:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-//your code here
 function majorityElement(nums) {
-  const n = nums.length;
-  const map = {};
+  let count = 0;
+  let candidate = null;
 
-  // create a frequency map of elements
-  for (let i = 0; i < n; i++) {
-    if (map[nums[i]]) {
-      map[nums[i]]++;
-    } else {
-      map[nums[i]] = 1;
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
     }
+    count += (num === candidate) ? 1 : -1;
   }
 
-  // find the element with frequency > n/2
-  for (let key in map) {
-    if (map[key] > Math.floor(n / 2)) {
-      return parseInt(key);
-    }
-  }
+  return candidate;
 }
-
-console.log(majorityElement([2, 1, 2])); // output: 2
